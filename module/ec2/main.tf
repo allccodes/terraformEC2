c1521b44dfc1f0fbd6287aef3871237af44e5068
Bastard Operator <bofh@MacBook-Pro.lan>
2023-07-27 22:47:55 +0100

    First commit

---
+++ module/ec2/main.tf
@@ -0,0 +1,19 @@
+# PROVIDER
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+# CREATE INSTANCE
+
+resource "aws_instance" "myInstance" {
+
+
+  ami           = var.linux
+  instance_type = "t2.micro"
+
+
+}
