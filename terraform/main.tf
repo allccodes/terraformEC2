0f056b4ce5bb73ed26c1a2932eb2218f05c383ac
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-01 16:57:06 +0100

    testing terraform@v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,15 @@ module "ec2" {
    inst_type = "t2.micro"
 }
 
+
+# Colocado para importar
 resource "aws_instance" "console" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
 }
 
 
+# Colocado para importar
 provider "aws" {
   region = "us-east-1"
 }
