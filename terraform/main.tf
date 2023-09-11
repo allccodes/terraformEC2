38b5c392eee2a9d79825706f471348d0637c5420
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-11 14:39:42 +0100

    module ec2 comment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-module "ec2" {
-   source    = "../module/ec2"
-   linux     = "ami-06ca3ca175f37dd66"
-   inst_type = "t2.micro"
-}
+# module "ec2" {
+#    source    = "../module/ec2"
+#    linux     = "ami-06ca3ca175f37dd66"
+#    inst_type = "t2.micro"
+# }
 
  provider "aws" {
    region = "us-east-1"
