704f67f0025c48bfc3c431f8b10498cbebf33838
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-11 16:42:32 +0100

    refactor bucket creation

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,4 +1,17 @@
-resource "aws_s3_bucket_acl" "example" {
-  bucket = var.bucket_name
-  acl = "private"
+
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = false
+
+  tags = {
+    Name        = "secure-bucket"
+    Environment = "Dev"
+  }
+
+}
+
+
+resource "aws_s3_bucket_acl" "secure_bucket_acl" {
+  bucket = aws_s3_bucket.secure_bucket.id
+  acl    = "private"
 }
