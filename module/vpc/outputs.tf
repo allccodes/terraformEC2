cb2446e79afc3aa3538987fc2b05d96ff1a6a26e
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-13 22:40:09 +0100

    output vpc_id

--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -2,7 +2,6 @@
 #   value = data.aws_availability_zones.available.names
 # }
 
-# output "vpc_id" {
-#      #value = aws_vpc.my-vpc.id
-#      value = module.vpc.vpc_id
-# }
+output "vpc_id" {
+     value = aws_vpc.my-vpc.id
+}
