b82899c98732f6e071c448cc67b69c9bccf3d525
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-13 22:19:37 +0100

    fix dynamo

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -40,23 +40,23 @@
 # }
 
 
-resource "aws_security_group" "public_instance_80" {
-  name        = "Public-instance"
-  description = "expose HTTP"
-  vpc_id      = module.vpc.vpc_id
-  ingress {
-    protocol        = "tcp"
-    from_port       = 22
-    to_port         = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "public_instance_80" {
+#   name        = "Public-instance"
+#   description = "expose HTTP"
+#   vpc_id      = module.vpc.vpc_id
+#   ingress {
+#     protocol        = "tcp"
+#     from_port       = 22
+#     to_port         = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
 
