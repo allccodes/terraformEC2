d74bb34dec8cfcd613ccef860c41aad81fa40b00
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-13 17:38:08 +0100

    add output to vpc modul9

--- module/s3/outputs.tf
+++ module/s3/outputs.tf
@@ -1,9 +1,3 @@
 output "return_arn" {
      value = "${aws_s3_bucket.myBucket.id}"
 }
-
-
-output "vpc_id" {
-     #value = aws_vpc.my-vpc.id
-     value = module.vpc.vpc_id
-}
--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -2,3 +2,7 @@
 #   value = data.aws_availability_zones.available.names
 # }
 
+output "vpc_id" {
+     #value = aws_vpc.my-vpc.id
+     value = module.vpc.vpc_id
+}
