8e664321179fb6facc82f831ee36ed3c6b399906
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-13 11:09:01 +0100

    test data for AZs

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,18 +1,8 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+resource "aws_subnet" "primary" {
+  availability_zone = data.aws_availability_zones.available.names[0]
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
-  tags = {
-    Terraform = "true"
-    Environment = "develop"
-  }
 }
