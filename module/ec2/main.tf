f326975f267bd35773c348e44a62f000ae34f93e
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-14 11:43:07 +0100

    create instance 19

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -22,42 +22,42 @@
 
 # # https://stackoverflow.com/questions/75797258/how-do-i-reference-a-resource-created-from-different-module-in-my-current-module
 
-# resource "aws_security_group" "public_instance_ssh" {
-#   name        = "Public-instance"
-#   description = "expose SSH"
-#   vpc_id      = module.vpc.vpc_id
-#   ingress {
-#     protocol        = "tcp"
-#     from_port       = 22
-#     to_port         = 22
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     protocol    = "-1"
-#     from_port   = 0
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_security_group" "public_instance_ssh" {
+  name        = "Public-instance"
+  description = "expose SSH"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    protocol        = "tcp"
+    from_port       = 22
+    to_port         = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
-# resource "aws_security_group" "public_instance_80" {
-#   name        = "Public-instance"
-#   description = "expose HTTP"
-#   vpc_id      = module.vpc.vpc_id
-#   ingress {
-#     protocol        = "tcp"
-#     from_port       = 22
-#     to_port         = 22
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     protocol    = "-1"
-#     from_port   = 0
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_security_group" "public_instance_80" {
+  name        = "Public-instance"
+  description = "expose HTTP"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    protocol        = "tcp"
+    from_port       = 22
+    to_port         = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
