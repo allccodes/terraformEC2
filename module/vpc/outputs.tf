84134826eab1c3e0cd76e3f2b3cba600c9743045
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-14 11:07:34 +0100

    create instance 11

--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -2,8 +2,7 @@
 #   value = data.aws_availability_zones.available.names
 # }
 
-# output "vpc_id" {
-#      value = module.vpc.vpc_id
-# }
-
+output "public_subnets" {
+  value = aws_subnet.public.*.id
+}
 
