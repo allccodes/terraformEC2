168fbd83c45d2ebc5ce353e0039dcac93c26b590
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-14 17:29:28 +0100

    remove all2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
-output "vpc_id" {
-    value = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#     value = module.vpc.vpc_id
+# }
 
 
-output "subnet_id" {
-  value = module.vpc.subnet_id
-}
+# output "subnet_id" {
+#   value = module.vpc.subnet_id
+# }
