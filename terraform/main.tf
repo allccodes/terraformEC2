65c945fb3f00a547342e97e0f32809907767d28c
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-14 10:33:28 +0100

    create instance 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ module "vpc" {
 
 
 module "ec2" {
-  depends_on = [module.vpc]
   source    = "../module/ec2"
+  depends_on = [module.vpc]
   linux     = "ami-06ca3ca175f37dd66"
   inst_type = "t2.micro"
 }
