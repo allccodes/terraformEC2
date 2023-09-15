f4ae60f0235bc4b0b6dd88d3cf30795135d03c00
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-15 16:38:59 +0100

    create target group

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-# output "vpc_id" {
-#     value = module.vpc.vpc_id
-# }
+output "vpc_id" {
+    value = module.vpc.vpc_id
+}
 
 
-# output "subnet_id" {
-#   value = module.vpc.subnet_id
-# }
+output "subnet_id" {
+  value = module.vpc.subnet_id
+}
 
 
 output "public_subnets" {
