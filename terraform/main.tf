8b61b4bd377c81aa3179f7e985f405af17e8eae3
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-15 15:11:48 +0100

    create target group

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,6 @@ module "vpc" {
 
 
 
-
-
-
 module "ec2" {
   source    = "../module/ec2"
   linux     = "ami-06ca3ca175f37dd66"
@@ -23,6 +20,7 @@ module "ec2" {
 
 
 module "elb" {
+  depends_on = [ module.ec2 ]
   source    = "../module/elb"
   public_subnets   = "${module.vpc.public_subnets}"
   vpc_id    = "${module.vpc.vpc_id}"
