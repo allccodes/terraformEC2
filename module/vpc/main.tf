fe75955d6e928429d7b5b4c5a6deae155d1d81d1
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-15 10:01:57 +0100

    rename vpc

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -7,7 +7,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "myVPC"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
