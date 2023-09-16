57dc3dbbfeabaefbe7c432e475a1733e911f11b6
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-16 20:46:42 +0100

    teste

--- module/ec2/outputs.tf
+++ module/ec2/outputs.tf
@@ -8,19 +8,9 @@
 # }
 
 
-data "aws_vpc" "example_vpc" {
-  id = "vpc-0982052aa15333394"
-}
 
 output "vpc_id" {
   value = data.aws_vpc.example_vpc.id
-}
-
-
-# Data source to fetch the subnet ID
-data "aws_subnet" "example_subnet" {
-  vpc_id = "vpc-0982052aa15333394"
-  cidr_block = "10.0.1.0/24"
 }
 
 out_put "subnet_id" {
