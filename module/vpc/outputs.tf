dc4e699bd0d1c140467885db57164bf496892009
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-16 22:37:40 +0100

    create alb

--- module/vpc/outputs.tf
+++ module/vpc/outputs.tf
@@ -2,16 +2,3 @@
 #   value = data.aws_availability_zones.available.names
 # }
 
-# output "vpc_id" {
-#   value = module.vpc.vpc_id
-# }
-
-
-output "subnet_id" {
-   value = module.vpc.public_subnets[0]
- }
-
-
- output "public_subnets" {
-   value = module.vpc.public_subnets[*]
- }
