0bca40049294ed3d2c8b3aa1967c4d8cecc5fedc
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-16 22:55:06 +0100

    create alb

--- module/elb/main.tf
+++ module/elb/main.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "example_vpc" {
 
 
 data "aws_subnet" "public_subnets" {
-  vpc_id = data.aws_vpc.example_vpc.id
+  vpc_id = "vpc-0f7be784bb4acb488"
 
   filter {
     name   = "tag:SubnetType"
