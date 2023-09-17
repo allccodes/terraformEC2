8310502b80d78fe843d802b833f5d052c9ae8603
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-17 15:42:10 +0100

    recreate instance3

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -14,7 +14,7 @@
 
 
 data "aws_instance" "example_instance" {
-  instance_id = "i-016fa4d3f614cefe5"
+  instance_id = "i-0def244fe01b53ebd"
 }
 
 
@@ -117,10 +117,10 @@ resource "aws_security_group" "public_instance_http" {
 
 # # # # ALLOCATE AWS_EIP TO INSTANCE
 
-# resource "aws_eip" "demo-eip" {
-#   instance = data.aws_instance.example_instance.id
-#   domain = "vpc"
-# }
+resource "aws_eip" "demo-eip" {
+  instance = data.aws_instance.example_instance.id
+  domain = "vpc"
+}
 
 
 
