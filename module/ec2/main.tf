a59b5708d981e3a199571800236e0334d3ddedcc
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-17 14:57:15 +0100

    create ec2 multiple

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,91 +1,114 @@
 
 
+# # Data source to fetch de vpc ID
+# data "aws_vpc" "example_vpc" {
+#   id = "vpc-04beae6fe5e39cf1b"
+# }
+
+# # Data source to fetch the subnet ID
+# data "aws_subnet" "example_subnet" {
+#   vpc_id = "vpc-04beae6fe5e39cf1b"
+#   cidr_block = "10.0.1.0/24"
+#   #cidr_block = "10.0.0.0/24"
+# }
+
+
+# data "aws_instance" "example_instance" {
+#   instance_id = "i-0c3d945c1316563d8"
+# }
+
+
 # Data source to fetch de vpc ID
 data "aws_vpc" "example_vpc" {
   id = "vpc-04beae6fe5e39cf1b"
 }
 
-# Data source to fetch the subnet ID
-data "aws_subnet" "example_subnet" {
-  vpc_id = "vpc-04beae6fe5e39cf1b"
-  cidr_block = "10.0.1.0/24"
-  #cidr_block = "10.0.0.0/24"
+# Data source to fetch the PUBLIC subnets
+data "aws_subnets" "example" {
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
 }
 
 
-data "aws_instance" "example_instance" {
-  instance_id = "i-0c3d945c1316563d8"
+resource "aws_instance" "app" {
+  for_each      = toset(data.aws_subnets.example.ids)
+  ami           = var.ami
+  instance_type = "t2.micro"
+  subnet_id     = each.value
 }
 
 
 
-# CREATE INSTANCE
 
-resource "aws_instance" "myInstance" {
-  ami           = var.linux
-  instance_type = var.inst_type
-  subnet_id     = data.aws_subnet.example_subnet.id
+# # CREATE INSTANCE
+
+# resource "aws_instance" "myInstance" {
+#   ami           = var.linux
+#   instance_type = var.inst_type
+#   subnet_id     = data.aws_subnet.example_subnet.id
   
-  vpc_security_group_ids = [aws_security_group.public_instance_ssh.id, aws_security_group.public_instance_http.id]
-  user_data              = <<EOF
-                            #!/bin/bash
-                            yum update -y
-                            yum install -y httpd
-                            systemctl start httpd
-                            EOF 
-  tags = {
-    Name = "Public Server"
-  }
-}
+#   vpc_security_group_ids = [aws_security_group.public_instance_ssh.id, aws_security_group.public_instance_http.id]
+#   user_data              = <<EOF
+#                             #!/bin/bash
+#                             yum update -y
+#                             yum install -y httpd
+#                             systemctl start httpd
+#                             EOF 
+#   tags = {
+#     Name = "Public Server"
+#   }
+# }
 
 
-# CREATE SECURITY GROUPS
+# # CREATE SECURITY GROUPS
 
-resource "aws_security_group" "public_instance_ssh" {
-  name        = "Public-instance-SSH"
-  description = "expose SSH"
-  vpc_id = data.aws_vpc.example_vpc.id
+# resource "aws_security_group" "public_instance_ssh" {
+#   name        = "Public-instance-SSH"
+#   description = "expose SSH"
+#   vpc_id = data.aws_vpc.example_vpc.id
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = 22
-    to_port         = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     protocol        = "tcp"
+#     from_port       = 22
+#     to_port         = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_security_group" "public_instance_http" {
-  name        = "Public-instance-HTTP"
-  description = "expose HTTP"
-  vpc_id     = data.aws_vpc.example_vpc.id
+# resource "aws_security_group" "public_instance_http" {
+#   name        = "Public-instance-HTTP"
+#   description = "expose HTTP"
+#   vpc_id     = data.aws_vpc.example_vpc.id
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = 80
-    to_port         = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     protocol        = "tcp"
+#     from_port       = 80
+#     to_port         = 80
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
-# # ALLOCATE AWS_EIP TO INSTANCE
+# # # ALLOCATE AWS_EIP TO INSTANCE
 
-resource "aws_eip" "demo-eip" {
-  instance = data.aws_instance.example_instance.id
-  domain = "vpc"
-}
+# resource "aws_eip" "demo-eip" {
+#   instance = data.aws_instance.example_instance.id
+#   domain = "vpc"
+# }
 
 
 
