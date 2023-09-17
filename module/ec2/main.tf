d3ee592ad84b915a92b64a6b83c8377c7603f7e4
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-17 15:24:24 +0100

    create ec2 multiple

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -13,9 +13,9 @@
 # }
 
 
-# data "aws_instance" "example_instance" {
-#   instance_id = "i-0c3d945c1316563d8"
-# }
+data "aws_instance" "example_instance" {
+  instance_id = "i-016fa4d3f614cefe5"
+}
 
 
 # Data source to fetch de vpc ID
@@ -115,10 +115,10 @@ resource "aws_security_group" "public_instance_http" {
 
 # # # ALLOCATE AWS_EIP TO INSTANCE
 
-# resource "aws_eip" "demo-eip" {
-#   instance = data.aws_instance.example_instance.id
-#   domain = "vpc"
-# }
+resource "aws_eip" "demo-eip" {
+  instance = data.aws_instance.example_instance.id
+  domain = "vpc"
+}
 
 
 
