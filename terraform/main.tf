6e1d522daad80255d8bf17a26ec4a068cafc6e70
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-18 11:54:51 +0100

    deleted subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,12 @@ module "vpc" {
 }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  linux     = "ami-06ca3ca175f37dd66"
-  inst_type = "t2.micro"
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   linux     = "ami-06ca3ca175f37dd66"
+#   inst_type = "t2.micro"
+#   depends_on = [module.vpc]
+# }
 
 
 # module "elb" {
