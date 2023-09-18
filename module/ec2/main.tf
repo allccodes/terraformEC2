8c2bba83e877bac482c9153f12fbe98ca264f3e1
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-18 10:56:00 +0100

    create instance teste

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "app" {
   ami           = var.linux
   instance_type = var.inst_type
   #subnet_id     = each.value
-  subnet_id = data.aws_subnet.example_vpc.id
+  subnet_id = data.aws_vpc.example_vpc.id
   vpc_security_group_ids = [aws_security_group.public_instance_ssh.id, aws_security_group.public_instance_http.id]
   user_data = <<EOF
     #!/bin/bash
