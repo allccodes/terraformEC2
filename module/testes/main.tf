118620b5cd102dbb24663bf18f4009df98afd0b7
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-20 16:53:38 +0100

    testing count

--- module/testes/main.tf
+++ module/testes/main.tf
@@ -11,6 +11,33 @@ data "aws_subnets" "example" {
   }
 }
 
+
+
+# CREATE INSTANCE
+
+resource "aws_instance" "app" {
+  count = length(data.aws_subnets.example)
+  #for_each      = toset(data.aws_subnets.example.ids)
+  ami           = var.linux
+  instance_type = var.inst_type
+  subnet_id = "${data.aws_subnets.example[count.index]}"
+  #subnet_id     = each.value
+  vpc_security_group_ids = [aws_security_group.public_instance_ssh.id, aws_security_group.public_instance_http.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+  tags = {
+    Name = "Public Server"
+  }
+}
+
+
+
+
 # # CREATE INSTANCE
 
 # resource "aws_instance" "app" {
