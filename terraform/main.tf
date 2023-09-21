aa6a3f6416944d7b24a6695519424875e48e9b6c
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-21 21:28:46 +0100

    create ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ module "vpc" {
 module "ec2" {
   source    = "../module/ec2"
   instance_number = 1
-  subnet_id = "vpc-*"
+  subnet_id = "vpc-0027b709979b1392e"
   linux     = "ami-06ca3ca175f37dd66"
   inst_type = "t2.micro"
   depends_on = [module.vpc]
