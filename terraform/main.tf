77ad7a42c625d344a419be56ac1eed4d6e92125e
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-21 17:50:23 +0100

    destroy all

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,23 +8,23 @@ provider "aws" {
 
 
 
-module "vpc" {
-   source    = "../module/vpc"
-}
+# module "vpc" {
+#    source    = "../module/vpc"
+# }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  linux     = "ami-06ca3ca175f37dd66"
-  inst_type = "t2.micro"
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   linux     = "ami-06ca3ca175f37dd66"
+#   inst_type = "t2.micro"
+#   depends_on = [module.vpc]
+# }
 
 
-module "elb" {
-  source    = "../module/elb"
-  depends_on = [module.vpc]
-}
+# module "elb" {
+#   source    = "../module/elb"
+#   depends_on = [module.vpc]
+# }
 
 
 
