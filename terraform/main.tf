89cb31ad198c3c9c9caec21731075ccbb503c123
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-21 23:27:51 +0100

    detroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,27 +5,27 @@ provider "aws" {
 
 
 
-module "vpc" {
-   source    = "../module/vpc"
-}
+# module "vpc" {
+#    source    = "../module/vpc"
+# }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  instance_number = 1
-  vpc_id = "vpc-0027b709979b1392e"
-  subnet_id = "subnet-05f9de789de5841b9"
-  linux     = "ami-06ca3ca175f37dd66"
-  inst_type = "t2.micro"
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   instance_number = 1
+#   vpc_id = "vpc-0027b709979b1392e"
+#   subnet_id = "subnet-05f9de789de5841b9"
+#   linux     = "ami-06ca3ca175f37dd66"
+#   inst_type = "t2.micro"
+#   depends_on = [module.vpc]
+# }
 
 
-module "elb" {
-  source    = "../module/elb"
-  vpc_id = "vpc-0027b709979b1392e"
-  depends_on = [module.vpc]
-}
+# module "elb" {
+#   source    = "../module/elb"
+#   vpc_id = "vpc-0027b709979b1392e"
+#   depends_on = [module.vpc]
+# }
 
 
 
