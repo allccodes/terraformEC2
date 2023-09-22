161bdb2cea8caea4c099a9166f637b8d48909361
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-22 17:44:11 +0100

    destroy2

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,20 +10,20 @@ module "vpc" {
 }
 
 
-# module "ec2" {
-#   source    = "../module/ec2"
-#   instance_number = 1
-#   linux     = "ami-06ca3ca175f37dd66"
-#   inst_type = "t2.micro"
-#   subnet_id = "subnet-010aa024febe40ee8"
-#   depends_on = [module.vpc]
-# }
-
-
-module "elb" {
-  source    = "../module/elb"
-  depends_on = [module.ec2]
+module "ec2" {
+  source    = "../module/ec2"
+  instance_number = 1
+  linux     = "ami-06ca3ca175f37dd66"
+  inst_type = "t2.micro"
+  subnet_id = "subnet-010aa024febe40ee8"
+  depends_on = [module.vpc]
 }
+
+
+# module "elb" {
+#   source    = "../module/elb"
+#   depends_on = [module.ec2]
+# }
 
 
 
