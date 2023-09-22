87ed65cd39b1208a2673f6e39c931d483c1d5a1a
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-22 14:29:35 +0100

    create vpc and ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ module "vpc" {
 module "ec2" {
   source    = "../module/ec2"
   instance_number = 1
-  vpc_id = "vpc-0beac3f140b678672"
+  #vpc_id = "vpc-0beac3f140b678672"
   subnet_id = "subnet-0f9441c4a35add5e9"
   linux     = "ami-06ca3ca175f37dd66"
   inst_type = "t2.micro"
