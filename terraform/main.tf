eec98b25ac6eddf76b91f221fa058b0a3fc06bac
Bastard Operator <bofh@MacBook-Pro.lan>
2023-09-23 22:00:07 +0100

    create vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,18 +10,18 @@ module "vpc" {
 }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  instance_number = 2
-  subnet_id = "subnet-014dd24cea4b30cf1"
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   instance_number = 2
+#   subnet_id = "subnet-014dd24cea4b30cf1"
+#   depends_on = [module.vpc]
+# }
 
 
-module "elb" {
-  source    = "../module/elb"
-  depends_on = [module.ec2]
-}
+# module "elb" {
+#   source    = "../module/elb"
+#   depends_on = [module.ec2]
+# }
 
 
 
