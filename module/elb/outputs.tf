42add10dc50761b1f3cb4303a24851deb5388b78
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-25 11:47:27 +0100

    create all

--- module/elb/outputs.tf
+++ module/elb/outputs.tf
@@ -1,6 +1,6 @@
 
 # OUTPUT LOAD BALANCER PUBLIC DNS
 
-output "load_balancer_public_ip" {
+output "elb_public_ip" {
   value = aws_elb.alb.dns_name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 
 # OUTPUT LOAD BALANCER PUBLIC DNS
 
-output "load_balancer_public_ip" {
-  value = module.elb.load_balancer_public_ip
+output "elb_public_ip" {
+  value = module.elb.elb_public_ip
 }
