044c89db70b7448f9706234c031992778d6f4d48
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-25 11:34:41 +0100

    create all

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -4,7 +4,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "var.vpc_name"
+  name = var.vpc_name
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
