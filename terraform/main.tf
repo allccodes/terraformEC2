f01e6f9108db2a346b1a5ccbfcbb660d3e515928
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-25 17:40:10 +0100

    delete all

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,29 +5,29 @@ provider "aws" {
 
 
 
-module "vpc" {
-   source    = "../module/vpc"
-   vpc_name = "myVPC"
-}
+# module "vpc" {
+#    source    = "../module/vpc"
+#    vpc_name = "myVPC"
+# }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  instance_number = 1
-  ami_id = "ami-092b99a36151a072e"
-  subnet_id = "subnet-02507c6cb9e739f80"
-  vpc_name = "myVPC"
-  subnet_number = 1
-  ingress2 = 80
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   instance_number = 1
+#   ami_id = "ami-092b99a36151a072e"
+#   subnet_id = "subnet-02507c6cb9e739f80"
+#   vpc_name = "myVPC"
+#   subnet_number = 1
+#   ingress2 = 80
+#   depends_on = [module.vpc]
+# }
 
 
-module "elb" {
-  source    = "../module/elb"
-  vpc_name = "myVPC"
-  depends_on = [module.vpc, module.ec2]
-}
+# module "elb" {
+#   source    = "../module/elb"
+#   vpc_name = "myVPC"
+#   depends_on = [module.vpc, module.ec2]
+# }
 
 
 
