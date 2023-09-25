e81a1dbfa10a789d985765b2c966cee0378f35f8
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-25 12:00:53 +0100

    create all

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,21 +11,21 @@ module "vpc" {
 }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  instance_number = 1
-  subnet_id = "subnet-02507c6cb9e739f80"
-  vpc_name = "myVPC"
-  ingress2 = 80
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   instance_number = 1
+#   subnet_id = "subnet-02507c6cb9e739f80"
+#   vpc_name = "myVPC"
+#   ingress2 = 80
+#   depends_on = [module.vpc]
+# }
 
 
-module "elb" {
-  source    = "../module/elb"
-  vpc_name = "myVPC"
-  depends_on = [module.ec2]
-}
+# module "elb" {
+#   source    = "../module/elb"
+#   vpc_name = "myVPC"
+#   depends_on = [module.ec2]
+# }
 
 
 
