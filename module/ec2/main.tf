0ca98c94536e6911a8f2594353708df2f157de97
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-25 15:37:20 +0100

    create all

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -9,6 +9,19 @@ data "aws_vpc" "my_vpc" {
 }
 
 
+
+
+data "aws_subnet" "my_subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+}
+
+
+
+
+
 # CREATE INSTANCE
 
 resource "aws_instance" "my_instance" {
@@ -16,7 +29,7 @@ resource "aws_instance" "my_instance" {
   ami           = var.ami_id
   instance_type = var.inst_type
 
-  subnet_id     = var.subnet_id
+  subnet_id     = data.aws_subnet.my_subnets.id[0]
   vpc_security_group_ids = [aws_security_group.public_instance_http.id]
 
   tags = {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,15 @@
 
+# output "private_subnets" {
+#   description = "List of IDs of private subnets"
+#   value       = module.vpc.private_subnets
+# }
+
+# output "public_subnets" {
+#   description = "List of IDs of public subnets"
+#   value       = module.vpc.public_subnets
+# }
+
+
 # OUTPUT LOAD BALANCER PUBLIC DNS
 
 output "elb_public_ip" {
