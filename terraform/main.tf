e139ba3399ad49df0f7a470b866b6e5e6cc54650
Pedro Almeida <pedro.almeida@aguasdecoimbra.pt>
2023-09-26 11:24:34 +0100

    delete ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,17 @@ module "vpc" {
 }
 
 
-module "ec2" {
-  source    = "../module/ec2"
-  instance_number = 1
-  ami_id = "ami-092b99a36151a072e"
-  subnet_id = "subnet-02507c6cb9e739f80"
-  vpc_name = "myVPC"
-  subnet_type = "private"
-  subnet_number = 1
-  ingress2 = 80
-  depends_on = [module.vpc]
-}
+# module "ec2" {
+#   source    = "../module/ec2"
+#   instance_number = 1
+#   ami_id = "ami-092b99a36151a072e"
+#   subnet_id = "subnet-02507c6cb9e739f80"
+#   vpc_name = "myVPC"
+#   subnet_type = "private"
+#   subnet_number = 1
+#   ingress2 = 80
+#   depends_on = [module.vpc]
+# }
 
 
 # module "elb" {
